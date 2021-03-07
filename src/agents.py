b94837a1213191a2c7401f296aafd5c9e270e08c
Lucas Hauptmann <lhauptmann@auvik.com>
2021-03-07 16:47:50 -0500

    Add the logic to move in the presence of another fish

--- src/agents.py
+++ src/agents.py
@@ -14,14 +14,33 @@ class TransitionIntensity:
         
 
 class Fish(Agent):
-    def __init__(self, unique_id, pos, model, transitionMatrix, moore=True):
+    def __init__(self, unique_id, pos, model, bold, transitionWhenAlone, moore=True):
         super().__init__(unique_id, model)
         self.pos = pos
         self.moore = moore
+        self.bold = bold
         self.transitionWhenAlone = TransitionIntensity(
             ["safe", "exposed"], 
-            [["SS", "SE"], ["ES", "EE"]], 
-            transitionMatrix
+            [
+                ["SS", "SE"], 
+                ["ES", "EE"]
+            ], 
+            transitionWhenAlone
+        )
+        self.transitionWhenPaired = TransitionIntensity(
+            ["both_safe", "bold_exposed_shy_safe", "bold_safe_shy_exposed", "both_exposed"], 
+            [
+                ["BS_BS", "BS_BESS", "BS_BSSE", "BS_BE"],
+                ["BESS_BS", "BESS_BESS", "BESS_BSSE", "BESS_BE"],
+                ["BSSE_BS", "BSSE_BESS", "BSSE_BSSE", "BSSE_BE"],
+                ["BE_BS", "BE_BESS", "BE_BSSE", "BE_BE"],
+            ], 
+            [
+                [0.864, 0.035, 0.101, 0],
+                [0.053, 0.918, 0, 0.029],
+                [0.101, 0, 0.812, 0.087],
+                [0, 0.03, 0.015, 0.955],
+            ]
         )
 
     def random_move(self):
@@ -50,16 +69,114 @@ class Fish(Agent):
             elif neighbour_type is Water:
                 waters.append(neighbour)
 
-        # For every fish around the current fish, in random order, try to move towards that fish
-        # if len(fishes) > 0:
-        #     self.random.shuffle(fishes)
-        #     for fish in fishes:
-        #         # Build the transition matrix
-        #         water = self.is_water_deep(fish.pos)
-        #         print(water)
+        self.random.shuffle(fishes)
+        self.random.shuffle(waters)
+
+        # For every fish around the current fish, in random order, try to move synced with that fish
+        if len(fishes) > 0:
+            for fish in fishes:
+                other_fish_in_deep_water = self.is_water_deep(fish.pos)
+                
+                # Select where to go randomly, in case we decide to go to a shallow or deep neighbour
+                shallow_neighbours = [w for w in waters if not w.deep]
+                shallow_neighbour = shallow_neighbours[0] if len(shallow_neighbours) > 0 else None
+                deep_neighbours = [w for w in waters if w.deep]
+                deep_neighbour = deep_neighbours[0] if len(deep_neighbours) > 0 else None
+                
+                if in_deep_water and other_fish_in_deep_water:
+
+                    change = np.random.choice(self.transitionWhenPaired.transitionName[0],replace=True,p=self.transitionWhenPaired.transitionMatrix[0])
+                    if change == "BS_BESS":
+                        if self.bold and not fish.bold:
+                            self.model.grid.move_agent(self, shallow_neighbour.pos)
+                            return
+
+                        elif not self.bold and fish.bold:
+                            self.model.grid.move_agent(fish, shallow_neighbour.pos)
+                            return
+
+                    elif change == "BS_BSSE":
+                        if self.bold and not fish.bold:
+                            self.model.grid.move_agent(fish, shallow_neighbour.pos)
+                            return
+                            
+                        elif not self.bold and fish.bold:
+                            self.model.grid.move_agent(self, shallow_neighbour.pos)     
+                            return
+                            
+                elif not in_deep_water and not other_fish_in_deep_water:
+                    
+                    change = np.random.choice(self.transitionWhenPaired.transitionName[3],replace=True,p=self.transitionWhenPaired.transitionMatrix[3])
+
+                    if change == "BE_BESS":
+                        if self.bold and not fish.bold:
+                            self.model.grid.move_agent(fish, deep_neighbour.pos)
+                            return
+                            
+                        elif not self.bold and fish.bold:
+                            self.model.grid.move_agent(self, deep_neighbour.pos)
+                            return
+                            
+                    elif change == "BE_BSSE":
+                        if self.bold and not fish.bold:
+                            self.model.grid.move_agent(self, deep_neighbour.pos)
+                            return
+                            
+                        elif not self.bold and fish.bold:
+                            self.model.grid.move_agent(fish, deep_neighbour.pos)
+                            return
+                            
+                else:
+                    if self.bold and not fish.bold:
+
+                        if in_deep_water and not other_fish_in_deep_water:
+
+                            change = np.random.choice(self.transitionWhenPaired.transitionName[2],replace=True,p=self.transitionWhenPaired.transitionMatrix[2])
+                            if change == "BSSE_BS":
+                                self.model.grid.move_agent(fish, deep_neighbour.pos)
+                                return
+                            
+                            elif change == "BSSE_BE":
+                                self.model.grid.move_agent(self, shallow_neighbour.pos)
+                                return
+                            
+
+                        elif not in_deep_water and other_fish_in_deep_water:
+
+                            change = np.random.choice(self.transitionWhenPaired.transitionName[1],replace=True,p=self.transitionWhenPaired.transitionMatrix[1])
+                            if change == "BESS_BS":
+                                self.model.grid.move_agent(self, deep_neighbour.pos)
+                                return
+
+                            elif change == "BESS_BE":
+                                self.model.grid.move_agent(fish, shallow_neighbour.pos)
+                                return
+                    
+                    elif not self.bold and fish.bold:
+
+                        if in_deep_water and not other_fish_in_deep_water:
+
+                            change = np.random.choice(self.transitionWhenPaired.transitionName[2],replace=True,p=self.transitionWhenPaired.transitionMatrix[1])
+                            if change == "BESS_BS":
+                                self.model.grid.move_agent(fish, deep_neighbour.pos)
+                                return
+
+                            elif change == "BESS_BE":
+                                self.model.grid.move_agent(self, shallow_neighbour.pos)
+                                return
+                        
+                        elif not in_deep_water and other_fish_in_deep_water:
+
+                            change = np.random.choice(self.transitionWhenPaired.transitionName[1],replace=True,p=self.transitionWhenPaired.transitionMatrix[2])
+                            if change == "BSSE_BS":
+                                self.model.grid.move_agent(self, deep_neighbour.pos)
+                                return
+
+                            elif change == "BSSE_BE":
+                                self.model.grid.move_agent(fish, shallow_neighbour.pos)
+                                return
 
         # If we didn't move towards any fish, try to move somewhere else, in random order
-        self.random.shuffle(waters)
         for water in waters:
             if water.deep != in_deep_water:
                 # If I'm looking at a water region with a different depth, use the transion matrix   
@@ -80,11 +197,12 @@ class Fish(Agent):
 
 class BoldFish(Fish):
     def __init__(self, unique_id, pos, model):
-        transitionMatrix = [
+        transitionWhenAlone = [
             [0.977,0.023],
             [0.031,0.969]
         ]
-        super().__init__(unique_id, pos, model, transitionMatrix)
+        bold = True
+        super().__init__(unique_id, pos, model, bold, transitionWhenAlone)
         self.pos = pos
 
     def step(self):
@@ -93,11 +211,12 @@ class BoldFish(Fish):
 
 class ShyFish(Fish):
     def __init__(self, unique_id, pos, model):
-        transitionMatrix = [
+        transitionWhenAlone = [
             [0.995,0.005],
             [0.038,0.962]
         ]
-        super().__init__(unique_id, pos, model, transitionMatrix)
+        bold = False
+        super().__init__(unique_id, pos, model, bold, transitionWhenAlone)
     
     def step(self):
         self.move()
